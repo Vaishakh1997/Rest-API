4de6dbe0e855dba70d26112d78203b2dacfd139e
Vaishakh1997 <vaishakh.devadiga@mountblue.tech>
2019-12-15 15:15:48 +0530

    Added index.js with refactored code

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const customers=require('./src/routes/customers.js')
-const port = process.env.PORT || 3000
+const orders = require('./src/routes/orders.js')
+const port = process.env.PORT || 3008
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true,}))
 
 app.use('/',customers)
+app.use('/',orders)
+app.use('*',(request,response) => response.send("Invalid URL"))
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+app.listen(port, () => console.log(`App running on port ${port}.`))
