273f507c194184604b865f9098118fd7606907bf
Vaishakh1997 <vaishakh.devadiga@mountblue.tech>
2019-12-15 15:18:39 +0530

    validation.js contains validation part of the project

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,27 +1,40 @@
 const Joi=require('joi')
 
+
 function validateId(id)
 {
-     var ob={};
-     ob['id']=id;
-     const schema = {
-          id: Joi.number().integer().less(50)
-      }
-      return Joi.validate(ob,schema)
+     const schema ={
+          id: Joi.number().positive().integer().required()}
+      return Joi.validate({'id':id},schema)
 }
 
-function validatePost()
+
+function validateBody(body)
 {
-     var ob={};
-     ob['id']=id;
      const schema = {
-          id: Joi.number().integer().less(50)
-      }
-      return Joi.validate(ob,schema)
+          name: Joi.string().min(5).max(18).required(),
+          phone: Joi.number().integer().min(10).precision(10).required(),
+          email: Joi.string().email().required(),
+          city: Joi.string().min(5).max(18).required(),
+          pincode: Joi.number().integer().min(6).precision(9).required(),
+      };
+      return Joi.validate(body,schema)
+}
+
+function validateBodyOrders(body)
+{
+     const schema = {
+          customer_id: Joi.number().positive().integer().max(25).required(),
+          order_status: Joi.string().required(),
+          order_type: Joi.string().required(),
+          amount: Joi.number().integer().required(),
+      };
+      return Joi.validate(body,schema)
 }
 
 
 module.exports = {
      validateId,
-     validatePost
+     validateBody,
+     validateBodyOrders
 }
