25073aa09f16c0ea9c4a322b2ed5db99d984b411
Vaishakh1997 <vaishakh.devadiga@mountblue.tech>
2019-12-15 15:31:35 +0530

    Added customers.js and orders.js

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -1,22 +1,22 @@
-const db = require("../models/customers.js");
+const db = require("../models/orders");
 const validate = require("../utils/validation.js")
 
 
-const getCustomers = (request, response, next) => {
-     db.getCustomers()
+const getOrders = (request, response, next) => {
+     db.getOrders()
           .then(data => { response.json(data.rows)})
           .catch(err => next(err));
 };
 
 
 
-const getCustomersById = (request, response, next) => {
+const getOrdersById = (request, response, next) => {
      const valResultId=validate.validateId(request.params.id)
      if(valResultId.error)
           response.status(400).send(valResultId.error.details[0].message)
      else{
-          const customer_id = parseInt(request.params.id)
-          db.getCustomersById(customer_id)
+          const order_id = parseInt(request.params.id)
+          db.getOrdersById(order_id)
                .then(data => {
                     if(data.rows[0] !=undefined)
                          response.json(data.rows)
@@ -28,35 +28,35 @@ const getCustomersById = (request, response, next) => {
 
 
 
-const createCustomers=(request, response, next) =>{
-     const valResultBody=validate.validateBody(request.body)
+const createOrders=(request, response, next) =>{
+     const valResultBody=validate.validateBodyOrders(request.body)
      if(valResultBody.error)
           response.status(400).send(valResultBody.error.details[0].message)
      else{
-          const {name,phone,email,city,pincode} = request.body
-          db.createCustomers(name,phone,email,city,pincode)
-               .then(data=>response.json(`Customer ${data.rows[0].name} is Added Successfully...`))
+          const {customer_id, order_status, order_type, amount} = request.body
+          db.createOrders(customer_id, order_status, order_type, amount)
+               .then(data => response.json(`Order ${data.rows[0].order_id} is Added Successfully...`))
                .catch(err => next(err));
      }
 }
 
 
 
-const updateCustomers=(request, response, next) =>{
+const updateOrders=(request, response, next) =>{
      const valResultId=validate.validateId(request.params.id)
      if(valResultId.error)
           response.status(400).send(valResultId.error.details[0].message)
      else{
-          const valResultBody=validate.validateBody(request.body)
+          const valResultBody=validate.validateBodyOrders(request.body)
           if(valResultBody.error)
                response.status(400).send(valResultBody.error.details[0].message)
           else{
-               const customer_id = parseInt(request.params.id)
-               const { name, email, phone, city, pincode } = request.body
-               db.updateCustomers(customer_id, name, email,phone,city,pincode)
+               const order_id = parseInt(request.params.id)
+               const { customer_id, order_status, order_type, amount } = request.body
+               db.updateOrders(order_id, customer_id, order_status, order_type, amount)
                     .then(data=>{
                          if(data.rows[0] !=undefined)
-                         response.json(`Customer ${data.rows[0].name} with ${data.rows[0].customer_id} Updated Successfully...`)
+                         response.json(`Order ${data.rows[0].order_id} with ${data.rows[0].customer_id} Updated Successfully...`)
                          else next()
                     })
           }
@@ -65,16 +65,16 @@ const updateCustomers=(request, response, next) =>{
 
 
 
-const deleteCustomers=(request, response, next) =>{
+const deleteOrders=(request, response, next) =>{
      const valResultId=validate.validateId(request.params.id)
      if(valResultId.error)
           response.status(400).send(valResultId.error.details[0].message)
      else{
-          const customer_id = parseInt(request.params.id)
-          db.deleteCustomers(customer_id)
+          const order_id = parseInt(request.params.id)
+          db.deleteOrders(order_id)
           .then(data=>{
                if(data.rows[0] !=undefined)
-               response.json(`Customer ${data.rows[0].name} with ID ${data.rows[0].customer_id} deleted Successfully...`)
+               response.json(`Order ${data.rows[0].order_id} with ID ${data.rows[0].customer_id} deleted Successfully...`)
                else next()
           })
      }
@@ -84,9 +84,9 @@ const deleteCustomers=(request, response, next) =>{
 
 
 module.exports = {
-     getCustomers,
-     getCustomersById,
-     createCustomers,
-     updateCustomers,
-     deleteCustomers
+     getOrders,
+     getOrdersById,
+     createOrders,
+     updateOrders,
+     deleteOrders
 };
