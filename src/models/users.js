01c4c5a69e757be8a7af34da010f035de0cf808a
Vaishakh1997 <vaishakh.devadiga@mountblue.tech>
2019-12-16 22:01:35 +0530

    Added users.js contains query part of the users table

--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,6 @@
 const pool=require("/home/vaishakh/rest/seedData.js")
 
+
 async function createUser(name, email, password){
   return await pool.query('INSERT INTO users(name, email, password) VALUES ($1, $2, $3) returning *', 
     [name,email,password])
@@ -8,7 +9,6 @@ async function createUser(name, email, password){
 async function validEmail(email){
     return await pool.query('SELECT * FROM users WHERE  email= $1 ',[email] )
   }
-
 
 
   module.exports={createUser,validEmail}
