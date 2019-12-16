41cf262b4cf83106e8a3df3efb74a2b40077186f
Vaishakh1997 <vaishakh.devadiga@mountblue.tech>
2019-12-16 22:09:13 +0530

    Added orders.js contains routing part

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,4 +6,5 @@ const middleware=require("../middleware/customers.js")
 router.post('/register', controllers.createUser, middleware.errorHandleEmail)
 router.post('/login', controllers.loginUser, middleware.errorHandleEmailAndPassword)
 
+
 module.exports=router;
