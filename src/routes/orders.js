9e1ed20584cf878f388af173f40131167510fbec
Vaishakh1997 <vaishakh.devadiga@mountblue.tech>
2019-12-17 20:05:38 +0530

    Added refactored code to orders.js, which contains routing part of the orders part

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -2,15 +2,15 @@ const express = require('express')
 const router = express.Router();
 const controllers = require('../controllers/orders.js')
 const middleware=require("../middleware/customers.js")
-
+const auth = require("../utils/tokenVerify")
 
 router.get('/', (request, response) => response.json({ info: 'Node.js, Express, and Postgres API' }))
 
-router.get('/orders', controllers.getOrders)
-router.get('/orders/:id', controllers.getOrdersById, middleware.errorHandleOrderId)
-router.post('/orders', controllers.createOrders, middleware.errorHandleCustomerId)
-router.put('/orders/:id', controllers.updateOrders, middleware.errorHandleOrderId)
-router.delete('/orders/:id', controllers.deleteOrders, middleware.errorHandleOrderId)
+router.get('/orders', auth.verifyToken, controllers.getOrders)
+router.get('/orders/:id', auth.verifyToken, controllers.getOrdersById, middleware.errorHandleOrderId)
+router.post('/orders', auth.verifyToken, controllers.createOrders, middleware.errorHandleCustomerId)
+router.put('/orders/:id', auth.verifyToken, controllers.updateOrders, middleware.errorHandleOrderId)
+router.delete('/orders/:id', auth.verifyToken, controllers.deleteOrders, middleware.errorHandleOrderId)
 
 
 module.exports=router;
