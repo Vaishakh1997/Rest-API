583a1e86b314828a570decaae8c2471f426f9a77
Vaishakh1997 <vaishakh.devadiga@mountblue.tech>
2019-12-17 20:04:55 +0530

    Added refactored code to customers.js, which contains routing part of the customers part

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -2,13 +2,14 @@ const express = require('express')
 const router = express.Router();
 const controllers = require('../controllers/customers.js')
 const middleware=require("../middleware/customers.js")
+const auth=require("../utils/tokenVerify")
 
 router.get('/', (request, response) => response.json({ info: 'Node.js, Express, and Postgres API' }))
 
-router.get('/customers', controllers.getCustomers)
-router.get('/customers/:id', controllers.getCustomersById, middleware.errorHandle)
-router.post('/customers', controllers.createCustomers)
-router.put('/customers/:id', controllers.updateCustomers, middleware.errorHandle)
-router.delete('/customers/:id', controllers.deleteCustomers, middleware.errorHandle)
+router.get('/customers', auth.verifyToken, controllers.getCustomers)
+router.get('/customers/:id', auth.verifyToken, controllers.getCustomersById, middleware.errorHandle)
+router.post('/customers', auth.verifyToken, controllers.createCustomers)
+router.put('/customers/:id', auth.verifyToken, controllers.updateCustomers, middleware.errorHandle)
+router.delete('/customers/:id', auth.verifyToken, controllers.deleteCustomers, middleware.errorHandle)
 
 module.exports=router;
