6846beb91f40631e1245c1322694a67a14f28cf6
Vaishakh1997 <vaishakh.devadiga@mountblue.tech>
2019-12-17 20:06:32 +0530

    Added refactored code to users.js, which contains routing part of the users part

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,7 @@ const controllers = require('../controllers/users')
 const middleware=require("../middleware/customers.js")
 
 router.post('/register', controllers.createUser, middleware.errorHandleEmail)
-router.post('/login', controllers.loginUser, middleware.errorHandleEmailAndPassword)
+router.post('/login', controllers.loginUser, middleware.errorHandleEmailAndPassword, )
 
 
 module.exports=router;
